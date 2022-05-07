b51e83a9111a618ddbcb5d1474f81e8e3ed76568
pigeonjim <51032754+pigeonjim@users.noreply.github.com>
2022-05-07 10:00:59 +0100

    Update and rename Game.java to GamePlay.java
    Refactor to add button pieces as a separate class and to put all the logic into the game play class

--- GamePlay.java
+++ GamePlay.java
@@ -2,18 +2,18 @@
 package ticTacToe;
 import java.util.Arrays;
 
-public class Game {
+public class GamePlay {
     private int turnsLeft;
-    private String[][] move;
+    private String[][] moves;
     private String currentPlayer;
     
-    public Game(){
+    public GamePlay(){
         this.turnsLeft = 9;
-        move = new String[3][3];
-        currentPlayer = "X";
+        moves = new String[3][3];
+        currentPlayer = "Turn: X";
         
         //inialise array to remove Nulls
-        for(String[] ary: move){
+        for(String[] ary: moves){
           Arrays.fill(ary, " ");  
         }       
     }
@@ -35,25 +35,34 @@ public class Game {
     }
 
     public void addMove(int row, int column) {
-       this.move[row][column] = this.currentPlayer;
+       this.moves[row][column] = getButtonPlayer();
+        //increment player
+        if(currentPlayer.equals("Turn: X")){
+            currentPlayer = "Turn: O";                            
+        } else {
+            currentPlayer = "Turn: X";
+        }
+        //update number of turns left
+        turnsLeft--;        
     }
     
-    public boolean checkFor3inaRow(String p){        
+    public boolean checkFor3inaRow(String p){  
+        //returns true if there is a winning line
         for(int i = 0; i < 3; i++){
             //check for vertical 3 in a row
-            if(this.move[i][0].equals(p) && this.move[i][1].equals(p) && this.move[i][2].equals(p) ){
+            if(this.moves[i][0].equals(p) && this.moves[i][1].equals(p) && this.moves[i][2].equals(p) ){
                 return true;
             }
             //check for horizontal 3 in a row
-            if(this.move[0][i].equals(p) && this.move[1][i].equals(p) && this.move[2][i].equals(p) ){
+            if(this.moves[0][i].equals(p) && this.moves[1][i].equals(p) && this.moves[2][i].equals(p) ){
                 return true;
             }
         }
         //check for diagonal 3 in a row
-        if(this.move[0][0].equals(p) && this.move[1][1].equals(p) && this.move[2][2].equals(p) ){
+        if(this.moves[0][0].equals(p) && this.moves[1][1].equals(p) && this.moves[2][2].equals(p) ){
                 return true;
             }
-        if(this.move[2][0].equals(p) && this.move[1][1].equals(p) && this.move[0][2].equals(p) ){
+        if(this.moves[2][0].equals(p) && this.moves[1][1].equals(p) && this.moves[0][2].equals(p) ){
                 return true;
             }                
         return false;
@@ -61,9 +70,9 @@ public class Game {
     
     public boolean checkForWinner(){
         if(checkFor3inaRow("X") || checkFor3inaRow("O")){
-            return true;
+            return true;//there is a winner
         }
-        return false;
+        return false; //no winner yet
     }
 
     public String getCurrentPlayer() {
@@ -74,5 +83,10 @@ public class Game {
         this.currentPlayer = currentPlayer;
     }
     
+    public String getButtonPlayer(){
+        //Returns X or O to populate button text
+        String[] bits = currentPlayer.split(" ");
+        return bits[1];
+    }
        
 }
