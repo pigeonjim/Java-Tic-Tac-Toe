b44293f99e88773b9464704d9a681e04ccfee1cb
pigeonjim <51032754+pigeonjim@users.noreply.github.com>
2022-05-07 10:04:41 +0100

    Update application.java

--- application.java
+++ application.java
@@ -3,33 +3,23 @@ package ticTacToe;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.text.Font;
-import javafx.scene.layout.HBox;
-
 
 public class TicTacToeApplication extends Application {
 
     public void start(Stage window){
         
-        Game thisGame = new Game();
-        gameBoard board = new gameBoard(thisGame); 
+        GamePlay thisGame = new GamePlay();
+        GameBoard board = new GameBoard(thisGame); 
 
-        BorderPane gameLayout = new BorderPane();
-        gameLayout.getChildren().add(board.getView());
+        //BorderPane gameLayout = new BorderPane();
+        //gameLayout.getChildren().add(board.getView());
         
         
-        Scene view = new Scene(gameLayout,300,300);
+        Scene view = new Scene(board.getView(),300,300);
                
         window.setScene(view);
         window.show();
-    }
-    
+    }   
 
 
           
